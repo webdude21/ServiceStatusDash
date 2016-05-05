ec05bbe46cad6e6df01149d013dca789e1ee1ed6
Dimo Petrov <dimo.petrov@sap.com>
2016-05-05 23:37:47 +0300

    extracted icon path constants

--- src/scripts/util/icon-update.js
+++ src/scripts/util/icon-update.js
@@ -1,18 +1,22 @@
 let storage = require('../services/storage');
+const FAIL_ICON = '../img/icons/fail48.png',
+  SUCCESS_ICON = '../img/icons/success48.png',
+  OK_ICON = '../img/icons/ok48.png';
+
 /**
  * Updates the action icon based on the services statuses
  * @param {Service} selectedServices
  */
 function updateIcon({ selectedServices }) {
-  let iconPath = '../img/icons/fail48.png';
+  let icon = FAIL_ICON;
 
   if (selectedServices.every(service => service.working)) {
-    iconPath = '../img/icons/success48.png';
+    icon = SUCCESS_ICON;
   } else if (selectedServices.some(service => service.working)) {
-    iconPath = '../img/icons/ok48.png';
+    icon = OK_ICON;
   }
 
-  chrome.browserAction.setIcon({ path: iconPath });
+  chrome.browserAction.setIcon({ path: icon });
 }
 
 module.exports = () => storage.loadOptions().then(updateIcon);
