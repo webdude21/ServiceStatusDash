acd0e60b9d6d359c70bc158d67d3b74c9cc9661e
webdude21 <dimo.petrof@gmail.com>
2016-05-06 19:36:02 +0300

    moved to alarms api (timeouts didn't work)

--- src/scripts/controllers/background.js
+++ src/scripts/controllers/background.js
@@ -1,9 +1,7 @@
 let update = require('../services/update-status');
-const updateFrequency = 1000 * 60; // one minute
 
-function updateForever() {
-  update();
-  setTimeout(updateForever, updateFrequency);
-}
+chrome.runtime.onStartup.addListener(() => {
+  chrome.alarms.create('updateAlarm', { periodInMinutes: 1 });
+});
 
-chrome.runtime.onStartup.addListener(updateForever);
+chrome.alarms.onAlarm.addListener(update);
